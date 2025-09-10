db466e6eaa75e189a0b6a581dbab1b3d4466c902
josehefferan <josehefferan@gmail.com>
2025-09-09 20:22:56 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 3000,
+    host: true
   },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  preview: {
+    port: 3000,
+    host: true
   }
 })
