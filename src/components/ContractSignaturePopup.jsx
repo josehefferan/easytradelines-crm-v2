83f7b0d8637a7e9699990148a9c76aec1312e168
josehefferan <josehefferan@gmail.com>
2025-09-23 17:29:38 -0700

    Update ContractSignaturePopup.jsx

--- src/components/ContractSignaturePopup.jsx
+++ src/components/ContractSignaturePopup.jsx
@@ -187,7 +187,7 @@ const downloadPDF = () => {
 
   const options = {
     margin: 0.5,
-    filename: \`Easy_Tradelines_Agreement_\${brokerData.first_name}_\${brokerData.last_name}.pdf\`,
+    filename: `Easy_Tradelines_Agreement_${brokerData.first_name}_${brokerData.last_name}.pdf`,
     image: { type: 'jpeg', quality: 0.98 },
     html2canvas: { scale: 2 },
     jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
