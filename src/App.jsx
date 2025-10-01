c9d28d70dc6cc9bad3fba66bae4d118816b3512d
josehefferan <josehefferan@gmail.com>
2025-10-01 09:17:16 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route 
           path="/panel" 
           element={
